3fd096566d6ce0aee478cb74a901d6490aa59e08
tarik_ceylan <tarikceylan@outlook.com.tr>
2023-03-04 22:10:59 +0300

    Complete Task 4

--- product-service/data/products.js
+++ product-service/data/products.js
@@ -1,42 +1,42 @@
-mockData = [
+mockProducts = [
   {
     description:
       'Logitech G300s Optical Gaming Mouse. Black. Adjustable DPI. 6 Configurable Buttons',
-    id: 'PTM0001',
+    id: 'c18e2e65-b0d3-441e-b4ce-b2a93c60ed0b',
     price: 39.99,
     title: 'Logitech G300s'
   },
   {
     description: 'Logitech M330 Optical Bluetooth Mouse. Black. ',
-    id: 'PTM0002',
+    id: 'bb8b3c5c-38eb-4bd6-8d55-6746a0e23c24',
     price: 21.99,
     title: 'Logitech M330'
   },
   {
     description: 'Asus WT245 Optical Bluetooth Mouse. Black. Adjustable DPI',
-    id: 'PTM0003',
+    id: '6f86999b-b1ee-4066-83d0-abd3466de508',
     price: 32.99,
     title: 'Asus WT245'
   },
   {
     description: 'Logitech G213 Gaming Keyboard. Black. RGB Lightning',
-    id: 'PTK0001',
+    id: '6d6f78c8-b9b8-4c40-990b-ac3040601fc5',
     price: 42.5,
     title: 'Logitech G123'
   },
   {
     description:
       'Razer Blackwidow V3 Green Switch Keyboard. Black. RGB Lightning',
-    id: 'PTK0002',
+    id: 'e8cad684-6cbc-467e-9cf2-b859afa9d2b6',
     price: 61.9,
     title: 'Razer Blackwidow V3'
   },
   {
     description: 'Logitech G773 Bluetooth Headest. 7.1 Surround. White',
-    id: 'PTH0001',
+    id: 'aaab594d-2de3-479a-9562-e5d3d3e01502',
     price: 15,
     title: 'Logitech G773'
   }
 ];
 
-module.exports = mockData;
+module.exports = mockProducts;
---
+++ product-service/data/stocks.js
@@ -0,0 +1,28 @@
+mockStocks = [
+  {
+    product_id: 'c18e2e65-b0d3-441e-b4ce-b2a93c60ed0b',
+    itemCount: 10
+  },
+  {
+    product_id: 'bb8b3c5c-38eb-4bd6-8d55-6746a0e23c24',
+    itemCount: 3
+  },
+  {
+    product_id: '6f86999b-b1ee-4066-83d0-abd3466de508',
+    itemCount: 7
+  },
+  {
+    product_id: '6d6f78c8-b9b8-4c40-990b-ac3040601fc5',
+    itemCount: 6
+  },
+  {
+    product_id: 'e8cad684-6cbc-467e-9cf2-b859afa9d2b6',
+    itemCount: 1
+  },
+  {
+    product_id: 'aaab594d-2de3-479a-9562-e5d3d3e01502',
+    itemCount: 12
+  }
+];
+
+module.exports = mockStocks;
